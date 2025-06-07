e4bd083617158c69941a773e5edfda4ac1606fe6
= <=>
2025-06-08 00:49:37 +0530

    environment updated.

---
+++ src/mcqgenerator/MCQGenerator.py
@@ -0,0 +1,8 @@
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from mcqgenerator.logger import logging
+from mcqgenerator.utils import read_file, get_table_data
+   
